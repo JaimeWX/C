723b8bdf34d2aaba985786447370d996985901a6
Jaime_WX <1261360793@qq.com>
2023-01-15 19:55:10 +0800

    #define

---
+++ define_.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+int main()
+{
+    int fahr, celsius;
+
+    fahr = LOWER;
+
+    while (fahr <= UPPER) 
+    {
+        celsius = 5 * (fahr-32) / 9; 
+        printf("%d\t%d\n", fahr, celsius); 
+        fahr = fahr + STEP;
+    }
+    return 0;
+}
